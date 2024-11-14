cdee2a600736ba9f2b2f10a9ffe158200fa9f41f
Ane <00001123648062SP@al.educacao.sp.gov.br>
2024-11-14 11:56:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quais são os Elementos da Harmonia?',
+    'Honestidade,Lealdade,Gentileza, Generosidade, Bom-Humor e Mágica'
+)
+
+criaCartao(
+    'História',
+    'O que é a Noite da Lareira Calorosa?',
+    'Uma noite que se comemora a união entre pôneis terrestres,pegasus e unicórnios'
+)
+
+criaCartao(
+    'História',
+    'Quem foi a primeira princesa do Império de Crystal?',
+    'Princesa Amore'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome da primeira princesa que nasceu como alicórnio?',
+    'Flurryheart'
+)
